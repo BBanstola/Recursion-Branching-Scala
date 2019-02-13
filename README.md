fec7161739978577994ec6779b67a3dbcbd059a5
Bidur_Banstola <bidur.banstola@gmail.com>
2019-02-13 12:08:19 +0545

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Simple programs to show the recursion and branching ways done in Scala, By the means of programs like
+    * maze problem - shortest oath finder
+    * permutation problem
+    * Tower of Hanoi
---
+++ src/permutation/permutation.scala
@@ -0,0 +1,19 @@
+/**
+  * Created by Own on 1/27/2019.
+  */
+
+
+def permute(nums:List[Int], f:List[Int]=>Unit, p:List[Int]):Unit={
+  if (nums.isEmpty)
+  {
+    f(p)
+  } else {
+    var before = List[Int]()
+    var after = nums
+    while (after.nonEmpty){
+      permute(before :::after.tail, f,after.head::p )
+      before ::= after.head
+      after = after.tail
+    }
+  }
+}
